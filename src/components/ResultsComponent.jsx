d5b6b0ba1af230955a3f49c114663472405a6ff7
Sohan jagadeesh <sjag0009@student.monash.edu>
2023-10-02 00:53:32 +1100

    sankey chart to pie chart conversion

--- src/components/ResultsComponent.jsx
+++ src/components/ResultsComponent.jsx
@@ -1,62 +1,64 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
+import { useState, useEffect } from "react";
 
 function ResultsComponent({ data }) {
     // Exclude total_monthly_cost from the labels and values arrays
     const labels = Object.keys(data).filter(key => key !== "total_monthly_cost");
     const values = labels.map(label => data[label]);
 
-    // Set total_monthly_cost as the only source node
-    const source = Array(values.length).fill(0);
-    const target = Array.from({ length: values.length }, (_, i) => i + 1);
     const formattedLabels = labels.map((label, index) => `${label.replace(/_/g, ' ')}: $${values[index]}`);
-    const linkColors = ['#FF6F61', '#C44D51', '#C70039', '#900C3F', '#581845'];
-    const nodeColors = ['#A5678E', '#FF6F61', '#C44D51', '#C70039', '#900C3F', '#581845'];
+    const colors = ['#FF6F61', '#C44D51', '#C70039', '#900C3F', '#581845'];
+    
+    const [pull, setPull] = useState(new Array(values.length).fill(0));
 
+    const handleHover = (data) => {
+        const newPull = new Array(values.length).fill(0);
+        newPull[data.points[0].pointNumber] = 0.1;
+        setPull(newPull);
+    };
+
+    const handleUnhover = () => {
+        setPull(new Array(values.length).fill(0));
+    };
 
     const trace = {
-    type: 'sankey',
-    orientation: 'h',
-    node: {
-        pad: 15,
-        thickness: 50, // Increase the thickness to make the bar wider
-        line: {
-            color: 'white',
-            width: 0.5,
+        type: 'pie',
+        hole: 0.4, // This makes it a donut chart
+        labels: formattedLabels,
+        values: values,
+        textinfo: 'label+percent',
+        insidetextorientation: 'radial',
+        marker: {
+            colors: colors.slice(0, values.length)
         },
-        label: [`Total Monthly Cost: $${data.total_monthly_cost}`, ...formattedLabels], // Use formattedLabels here
-        color: nodeColors.slice(0, values.length), // Set the first color to emerald for the total monthly cost
-    },
-    link: {
-        source: source,
-        target: target,
-        value: values,
-        color: linkColors.slice(0, values.length), // Use the linkColors array here
-    },
-};
+        pull: pull
+        
+    };
 
-const layout = {
-    title: 'Estimated Expenditure Sankey Diagram',
-    font: {
-        size: 10,
-        color: 'white',
-    },
-    paper_bgcolor: 'black',
-};
+    const layout = {
+        // title: 'Estimated Expenditure Donut Diagram',
+        font: {
+            size: 10,
+            color: 'white',
+        },
+        paper_bgcolor: 'rgba(0, 0, 0, 0.2)',
+        width: 2 * 450,  // Assuming the original width was 450
+        height: 1.5 * 450
+    };
+
     
-    
-    
-    
-    
+
     return (
         <div>
             <div className="results-panel">
-            <p className=" text-center">
-                <span className="text-gradient text-2xl">Your estimated cost of living in Victoria is </span> 
-                <br/><span role="img" aria-label="dollar bill icon" style={{ fontSize: '1.5em', color: 'initial' }}>💵</span> 
-                <span className="text-gradient text-3xl font-bold"> ${data.total_monthly_cost}</span>
-            </p>
-        </div>
+                <p className="text-center">
+                    <br/>
+                    <span className="text-gradient text-2xl">Your estimated cost of living in Victoria is </span>
+                    <br /><span role="img" aria-label="dollar bill icon" style={{ fontSize: '1.5em', color: 'initial' }}>💵</span>
+                    <span className="text-gradient text-3xl font-bold"> ${data.total_monthly_cost}</span>
+                </p>
+            </div>
             <Plot data={[trace]} layout={layout} />
         </div>
     );
